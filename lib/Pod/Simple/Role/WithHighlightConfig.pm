a4fafa218719387a735d81f7bf0a41c42afa28bc
Graham Knop <haarg@haarg.org>
2014-09-18 06:10:58 -0400

    config validation and bare language

--- lib/Pod/Simple/Role/WithHighlightConfig.pm
+++ lib/Pod/Simple/Role/WithHighlightConfig.pm
@@ -40,6 +40,7 @@ around _handle_text => sub {
   }
 };
 
+
 around _handle_element_end => sub {
   my $orig = shift;
   my $self = shift;
@@ -48,10 +49,35 @@ around _handle_element_end => sub {
   if ($element eq 'for' and $self->_has_highlight_config_text) {
     my $text = $self->_highlight_config_text;
     $self->_clear_highlight_config_text;
-    my @config = split /(\S+)\s*=/, $text;
-    shift @config;
-    s/^\s+//, s/\s+$// for @config;
-    $self->_highlight_config({ @config });
+    s/^\s+//, s/\s+$// for $text;
+    my $config = {};
+    for my $config_item (map { [ split /=/, $_, 2 ] } split /\s+/, $text) {
+      my ($key, $value) = @$config_item;
+      if ($key =~ /^(?:start_line|highlight|line_numbers|language)$/) {
+        if(!defined $value || !length $value) {
+          $self->whine($item->{start_line}, "Invalid empty $key setting.")
+        }
+        elsif ($key eq 'start_line' && $value !~ /^\d+$/) {
+          $self->whine($item->{start_line}, "Invalid non-number ($value) for $key setting.")
+        }
+        elsif ($key eq 'highlight' && $value !~ /^\d+(?:-\d+)?(?:,\d+(?:-\d+)?)*$/) {
+          $self->whine($item->{start_line}, "Invalid number sequence ($value) for $key setting.")
+        }
+        elsif ($key eq 'line_numbers' && $value !~ /^[01]$/) {
+          $self->whine($item->{start_line}, "Invalid boolean ($value) for $key setting.")
+        }
+        else {
+          $config->{$key} = $value;
+        }
+      }
+      elsif (!defined $value) {
+        $config->{language} = $key;
+      }
+      else {
+        $self->whine($item->{start_line}, "Invalid setting \"$key\".")
+      }
+    }
+    $self->_highlight_config($config);
   }
   else {
     $self->$orig(@_);
--- t/basic.t
+++ t/basic.t
@@ -17,10 +17,21 @@ like $html,
   qr{<pre><code class="language-perl">  my \$var = foo\(\);</code></pre>},
   'language config set in class';
 like $html,
-  qr{<pre><code>  Just a verbatim section.</code></pre>},
+  qr{<pre><code>  Just a verbatim section</code></pre>},
   'empty language config resets settings';
 like $html,
-  qr{<pre data-start="5" data-line="1,4-10,20" class="line-numbers"><code class="language-javascript">  Another verbatim section.</code></pre>},
+  qr{<pre data-start="5" data-line="1,4-10,20" class="line-numbers"><code class="language-javascript">  Another verbatim section</code></pre>},
   'start line, line numbers, highlight lines set properly';
+like $html,
+  qr{<pre><code class="language-javascript">  Another verbatim section 2</code></pre>},
+  'bare language';
+
+like $html,
+  qr{<dt>Around line 29:</dt>\s*<dd>\s*<p>Invalid empty line_numbers setting\.</p>},
+  'errors for invalid line_numbers';
+
+like $html,
+  qr{<dt>Around line 33:</dt>\s*<dd>\s*<p>Invalid setting &quot;welp&quot;\.</p>},
+  'errors for invalid settings';
 
 done_testing;
